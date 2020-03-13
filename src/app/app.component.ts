7ad7d6d37d4711fdbf4be3d871a139dc0267a718
AnaMore21 <anastasiakrut1@gmail.com>
2020-03-13 16:00:41 +0200

    todo

---
+++ src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('moveInLeft', [
+      transition('void=> *', [style({transform: 'translateX(300px)'}),
+        animate('200ms ease-out', keyframes([
+          style({transform: 'translateX(300px)'}),
+          style({transform: 'translateX(0)'})
+
+        ]))]),
+      transition('*=>void', [style({transform: 'translateX(0px)'}),
+        animate('250ms ease-in',   keyframes([
+          style({transform: 'translateY(-20px)', opacity: 1, offset: 0.2}),
+          style({transform: 'translateY(250px)', opacity: 0 , offset: 1})
+
+        ]))])
+
+    ])
+  ]
+})
+export class AppComponent implements OnInit{
+  todoArray: {text: string, isDone: boolean}[] = [];
+
+  public form: FormGroup;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.constructForm();
+  }
+
+  constructForm() {
+    this.form = this.fb.group({
+      todo: this.fb.control(null, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) { return; }
+    this.todoArray.push({text: this.form.get('todo').value, isDone: false});
+    this.form.reset();
+  }
+
+  onDeleteItem(index) {
+    this.todoArray.splice(index, 1);
+  }
+
+  onDoneItem(index) {
+    this.todoArray[index].isDone = true;
+  }
+}
+
+
+
